06d164e2f4835ab6b88ccdd9af9499147105f23d
GyuminLee <gyumin.lee@rwth-aachen.de>
2017-06-23 12:31:05 +0200

    add Dropdown

--- WebContent/js/function.js
+++ WebContent/js/function.js
@@ -161,6 +161,31 @@ function query(userName) {
     };
 	sendRequest(functionType.QUERY, json);
 }
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction(dropdownID) {
+    document.getElementById(dropdownID).classList.toggle('show');
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function setUserinDropdown(userID, funcName) {
+	document.getElementById(funcName).value = userID;
+	document.getElementById(funcName).innerHTML =userID;
+}
 /*
 function addUser(userName, amount) {
 	var json =
